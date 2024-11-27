44d0bf3d1468923aa48c7573468f0e7d07778a6c
mouskit2 <mtraore@live.fr>
2024-11-27 15:57:07 +0100

    test 31

--- webpack.config.js
+++ webpack.config.js
@@ -34,14 +34,14 @@ module.exports = {
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'src/random-names.js', to: 'random-names.js' },
-        { from: 'src/script.js', to: 'script.js' },
-        { from: 'src/style.css', to: 'style.css' },
-        { from: 'src/fiche.html', to: 'fiche.html' },
-        { from: 'src/fiche_style.css', to: 'fiche_style.css' },
-        { from: 'src/menu-style.css', to: 'menu-style.css' },
-        { from: 'src/images', to: 'images' },
-
+        { from: 'src/random-names.js', to: 'dist/random-names.js' },
+        { from: 'src/random-fiche.js', to: 'dist/random-fiche.js' },
+        { from: 'src/script.js', to: 'dist/script.js' },
+        { from: 'src/style.css', to: 'dist/style.css' },
+        { from: 'src/fiche.html', to: 'dist/fiche.html' },
+        { from: 'src/fiche_style.css', to: 'dist/fiche_style.css' },
+        { from: 'src/menu-style.css', to: 'dist/menu-style.css' },
+        { from: 'src/images', to: 'dist/images' },
       ],
     }),
     new webpack.DefinePlugin({
@@ -54,5 +54,7 @@ module.exports = {
     }, 
     compress: true, 
     port: 9000, 
-    proxy: { '/env': 'http://localhost:3000' } },
+    proxy: { '/env': 'http://localhost:3000' } 
+  },
 };
+
